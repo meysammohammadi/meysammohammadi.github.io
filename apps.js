b87207d8e843045be1181b41eff11caf3757e4ba
Meysam <45339656+meysammohammadi@users.noreply.github.com>
2025-07-19 09:18:58 -0400

    Delete apps.js

--- apps.js
+++
@@ -1,8 +0,0 @@
-let root = document.documentElement;
-let range = document.getElementById('volume')
-let rangeVal = document.getElementById('range-val')
-
-range.addEventListener ('input', () => {
-    rangeVal.innerHTML = range.value
-    root.style.setProperty('--color', range.value)
-})
